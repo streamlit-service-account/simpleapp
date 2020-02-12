c1fb0d433bb8f9bff632cabb8bb802e29d924bfc
Matteo <monchier@users.noreply.github.com>
2020-02-12 15:38:50 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,4 @@ import streamlit as st
 
 st.title("Test App!!!")
 
-w1 = st.slider("Label 1", 0, 100, 25, 1)
-st.write("Value 1:", w1)
-
-w2 = st.slider("Label 2", 0.0, 100.0, (25.0, 75.0), 0.5)
-st.write("Value 2:", w2)
 
