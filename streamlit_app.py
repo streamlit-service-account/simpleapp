dcccf5a9920868b9e8e9a840798175fec022724d
Matteo <monchier@users.noreply.github.com>
2020-03-02 09:22:18 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,13 @@ import streamlit as st
 st.title("Test App!!!")
 
 value = st.slider("Pick a number", 0, 10, 3)
+
 st.write(value)
 
+input = st.text_input("Tell me something", "Cantami o Diva")
+
+with open("temp_file.txt", "a") as f:
+  f.write(f"{input}\n")
+  
+with open("temp_file.txt", "r") as f:
+  st.write(f.read())
