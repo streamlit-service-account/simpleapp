9e175f9681304840378e85d98ba6e73fea957f32
streamlit-service-account <75586618+streamlit-service-account@users.noreply.github.com>
2021-02-04 16:48:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,5 @@ import streamlit as st
 
 st.title("Testing Apps!")
 
-file_obj = st.sidebar.file_uploader('Choose an image:', ('jpg', 'jpeg'))
-
-value = st.slider("Pick a number", 0, 15, 3)
-
-st.write(value)
-
-input = st.text_input("Tell me something", "Cantami o Diva")
-
-with open("temp_file.txt", "a") as f:
-  f.write(f"{input}\n")
-  
-with open("temp_file.txt", "r") as f:
-  st.write(f.read())
-
-st.write("Streamlit is fabulous")
-
-st.write("Hello Corey, this is the demo!")
+st.write("Streamlit app is deployed!")
 st.balloons()
-
-print("this is a log line")
-
-uploaded_file = st.file_uploader("Choose a Jpg file", type="jpg")
-if uploaded_file is not None:
-    st.write(uploaded_file)
